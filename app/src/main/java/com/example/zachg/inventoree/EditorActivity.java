229b5b48e254d739200feb96064177443c650ddf
zachsg <zsgott@gmail.com>
2017-04-10 21:04:44 -0700

    Added UI scaffolding (menu options) to wipe data (in MainActivity) and delete individual product in detail view (EditorActivity).

--- app/src/main/java/com/example/zachg/inventoree/EditorActivity.java
+++ app/src/main/java/com/example/zachg/inventoree/EditorActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -61,6 +63,28 @@ public class EditorActivity extends AppCompatActivity
         } else {
             getSupportActionBar().setTitle(R.string.editor_title_new);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.editor_action_delete) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void cancel(View view) {
