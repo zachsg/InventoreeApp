e34079982696ec2904491ccca72dc711a5056508
zachsg <zsgott@gmail.com>
2017-04-13 23:06:53 -0700

    Removed a couple of helper methods no longer needed (bitmap -> string and vice versa).

--- app/src/main/java/com/example/zachg/inventoree/EditorActivity.java
+++ app/src/main/java/com/example/zachg/inventoree/EditorActivity.java
@@ -431,25 +431,4 @@ public class EditorActivity extends AppCompatActivity
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) { }
-
-    /**
-     * @return bitmap (from given string)
-     */
-    public Bitmap stringToBitMap(String encodedString){
-        try {
-            byte [] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
-            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-            return bitmap;
-        } catch(Exception e) {
-            e.getMessage();
-            return null;
-        }
-    }
-
-    public String bitMapToString(Bitmap bitmap){
-        ByteArrayOutputStream baos = new  ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-        byte [] b = baos.toByteArray();
-        return Base64.encodeToString(b, Base64.DEFAULT);
-    }
 }
